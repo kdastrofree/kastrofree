2710e2cbd194ab7ce4e5cd13b844b35a83b3dfc3
kdastrofree <kdastrofree@gmail.com>
2014-03-13 18:10:49 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kdastrofree::Application.config.secret_key_base = '92f30692975a77b9339f764eaa1850c3075dbcdf2b85a74106e241a392f2baf3d9a5ee4306ad8e46a393ba6594f31d84c5296826cbed9deb74e8974a90b32052'
